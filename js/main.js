c3939c959ce6df0e53a645dc4a264a2faeb97d09
Jay Freestone <mail@jayfreestone.com>
2015-02-14 18:39:38 +0000

    Added legend

--- js/main.js
+++ js/main.js
@@ -28,25 +28,44 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   ];
 
-  var tooltip = d3.select('body').append('div')
+  var nameToClass = function(d){return d.name.replace(/\s+/g, '-').toLowerCase()};
+
+  var tooltip = d3.select('.container').append('div')
       .classed('tooltip', true)
       .html('Hover over a group');
 
+  // var ratingPop = d3.select('body').append('div')
+  //   .classed('rating-tooltip', true);
+
   var arc =  function(i){
-    return d3.svg.arc()
-      .innerRadius(i * 50)
-      .outerRadius(i * 60)
-      .startAngle(0 * (Math.PI/180))
-      .endAngle(72 * (Math.PI/180));
+    if (i === 1){
+      return d3.svg.arc()
+        .innerRadius(0)
+        .outerRadius(60)
+        .startAngle(0 * (Math.PI/180))
+        .endAngle(72 * (Math.PI/180))
+
+    } else {
+
+      return d3.svg.arc()
+        .innerRadius(1 * 50)
+        .outerRadius(1 * 60)
+        .startAngle(0 * (Math.PI/180))
+        .endAngle(72 * (Math.PI/180))
+
+    }
   };
 
   var pathGroup = d3.select('#chart')
-    .style('width', 800)
-    .style('height', 800)
     .append('svg')
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 600 600")
     .append('g')
     .selectAll('path').data(areas)
-      .enter().append('g');
+      .enter().append('g')
+      .attr('class', function(d){
+        return nameToClass(d);
+      })
 
   pathGroup.on('mouseover', function(d){
     tooltip.style('background', d.color)
@@ -55,12 +74,35 @@ document.addEventListener('DOMContentLoaded', function(){
 
   for(var x = 1; x < 6; x ++){
     pathGroup.append('path')
+      .classed(x, true)
       .attr('d', arc(x))
       .style('fill', function(d){
         return d.color;
       })
       .attr('transform', function(d, i){
-        return  'translate(300, 150) ' + 'rotate(' + (i * 72) + ')';
+        return  'translate(300, 300) ' + 'rotate(' + (i * 72) + ') ' + 'scale('+x+', '+ x +')';
+      })
+      .on('mouseover', function(d){
+        // ratingPop.html(this.classList)
+        //   .style('left', (d3.event.pageX - 100) + 'px')
+        //   .style('top',  (d3.event.pageY - 100) + 'px')
+        //   .style('display', 'block')
+      })
+      .on('mouseout', function(d){
+        // ratingPop.style('display', 'none')
+      })
+      .on('click', function(d){
+        var name = '.' + nameToClass(d) + ' .selected';
+        // console.log(document.getElementsByName(nameToClass(d)+'__text'));
+        pathGroup.selectAll(name).classed('selected', false)
+
+        // if(this.classList.contains('selected') === true){
+        //   this.classList.remove('selected');
+        // } else {
+          document.getElementsByName(nameToClass(d)+'__text')[this.classList -1 ].checked = true;
+          d3.select(this).classed('selected', true);
+        // }
+        
       })
   }
 });
