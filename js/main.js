45e47e0f474d5811be5f3ba8b37e61158f096c1a
Jay Freestone <mail@jayfreestone.com>
2015-02-15 11:09:18 +0000

    Helen fixed the circles

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+
   var areas = [
     {
       name: 'Risk Management',
       color: 'purple',
+      colors: ['#DE9AC5', '#D570B1', '#CA4A9C', '#922C6E', '#5E1C47'],
       tooltext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
     {
       name: 'Client Management',
       color: 'blue',
+      colors: ['#71B9E9', '#3A9CDF', '#2582C4', '#1C6597', '#16527A'],
       tooltext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
     {
       name: 'Collaboration',
-      color: 'orange',
+      colors: ['#FCCDB6', '#F9AB86', '#F88A58', '#F6692F', '#DC511D'],
       tooltext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
     {
       name: 'Personal Effectiveness',
       color: 'green',
+      colors: ['#D6EAB8', '#B9DB85', '#A2CF5F', '#8BC041', '#78A538'],
       tooltext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
     {
       name: 'Finance Management',
-      color: 'grey',
+      colors: ['#CCCCCC', '#B2B2B2', '#999999', '#6E6E6E', '#545454'],
       tooltext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     }
   ];
 
   var nameToClass = function(d){return d.name.replace(/\s+/g, '-').toLowerCase()};
 
-  var tooltip = d3.select('.container').append('div')
+  var tooltip = d3.select('.explanation').append('div')
       .classed('tooltip', true)
       .html('Hover over a group');
 
@@ -41,15 +45,22 @@ document.addEventListener('DOMContentLoaded', function(){
     if (i === 1){
       return d3.svg.arc()
         .innerRadius(0)
-        .outerRadius(60)
+        .outerRadius(100)
         .startAngle(0 * (Math.PI/180))
         .endAngle(72 * (Math.PI/180))
 
-    } else {
-
+    } 
+    // else if (i === 6) {
+    //   return d3.svg.arc()
+    //     .innerRadius(0)
+    //     .outerRadius(500)
+    //     .startAngle(0 * (Math.PI/180))
+    //     .endAngle(72 * (Math.PI/180))
+    // } 
+    else {
       return d3.svg.arc()
-        .innerRadius(1 * 50)
-        .outerRadius(1 * 60)
+        .innerRadius(i * 50)
+        .outerRadius((i * 50)+50)
         .startAngle(0 * (Math.PI/180))
         .endAngle(72 * (Math.PI/180))
 
@@ -68,41 +79,45 @@ document.addEventListener('DOMContentLoaded', function(){
       })
 
   pathGroup.on('mouseover', function(d){
-    tooltip.style('background', d.color)
-    tooltip.html('<h3>' + d.name + '</h3>' + d.tooltext);
+    // d3.select(this).attr('transform', 'translate(-100,-100)')
+    d3.selectAll('path').style('opacity', '.5')
+    d3.select(this).selectAll('path').style('opacity', '1')
+    tooltip.style('background', d.colors[2])
+    tooltip.html('<h3>' + d.name + '</h3>' + '<p>' + d.tooltext + '</p>');
+  }).on('mouseout', function(){
+    d3.selectAll('path').style('opacity', '1')
+  }).on('click', function(){
+    this.classList.add('group--selected');
   })
 
+  var listItems = document.querySelectorAll('.legend li');
   for(var x = 1; x < 6; x ++){
     pathGroup.append('path')
       .classed(x, true)
       .attr('d', arc(x))
       .style('fill', function(d){
-        return d.color;
+        return d.colors[x-1];
       })
       .attr('transform', function(d, i){
-        return  'translate(300, 300) ' + 'rotate(' + (i * 72) + ') ' + 'scale('+x+', '+ x +')';
+        // return  'translate(300, 300) ' + 'rotate(' + (i * 72) + ') ' + 'scale('+ x +', '+ x +')';
+        return  'translate(300, 300) ' + 'rotate(' + (i * 72) + ') ';
       })
       .on('mouseover', function(d){
-        // ratingPop.html(this.classList)
-        //   .style('left', (d3.event.pageX - 100) + 'px')
-        //   .style('top',  (d3.event.pageY - 100) + 'px')
-        //   .style('display', 'block')
+        for(var c = 0; c < listItems.length; c++){
+          listItems[c].style.opacity = 0.3;
+        }
+        listItems[this.classList - 1].style.opacity = 1;
       })
       .on('mouseout', function(d){
-        // ratingPop.style('display', 'none')
+        for(var c = 0; c < listItems.length; c++){
+          listItems[c].style.opacity = 1;
+        }
       })
       .on('click', function(d){
         var name = '.' + nameToClass(d) + ' .selected';
-        // console.log(document.getElementsByName(nameToClass(d)+'__text'));
         pathGroup.selectAll(name).classed('selected', false)
-
-        // if(this.classList.contains('selected') === true){
-        //   this.classList.remove('selected');
-        // } else {
-          document.getElementsByName(nameToClass(d)+'__text')[this.classList -1 ].checked = true;
-          d3.select(this).classed('selected', true);
-        // }
-        
+        document.getElementsByName(nameToClass(d)+'__text')[this.classList -1 ].checked = true;
+        d3.select(this).classed('selected', true);
       })
   }
 });
